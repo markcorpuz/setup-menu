ef3a9f615a3d23e13d03e79d5072fc43a29d56bb
Mark Corpuz <social@markcorpuz.com>
2020-08-07 12:42:34 -0700

    SETUP-BE | Fixed the broken author link from items

--- inc/items.php
+++ inc/items.php
@@ -289,11 +289,13 @@ function setup_be_date() {
  * Jan 1, 2020 by Author
  * 
  */
-function setup_be_dateauthor() {
-	echo '<div class="item dateauthor">' . get_the_date( 'M j, Y' ) . ' by <a href="' . get_author_posts_url( get_the_ID() ) . '">' . get_the_author() . '</a></div>';
+function setup_be_dateauthor() {	
+	$author_id = get_post_field( 'post_author', get_the_ID() );
+	echo '<div class="item dateauthor">' . get_the_date( 'M j, Y' ) . ' by <a href="' . get_author_posts_url( $author_id ) . '">' . get_the_author_meta( 'display_name' , $author_id ) . '</a></div>';
 }
 function setup_be_dateauthor_nolink() {
-	echo '<div class="item dateauthor nolink">' . get_the_date( 'M j, Y' ) . ' by ' . get_the_author() . '</div>';
+	$author_id = get_post_field( 'post_author', get_the_ID() );
+	echo '<div class="item dateauthor nolink">' . get_the_date( 'M j, Y' ) . ' by ' . get_the_author_meta( 'display_name' , $author_id ) . '</div>';
 }
 
 
